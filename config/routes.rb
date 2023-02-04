6e467156c233bd77816ef70b4436e1c1f00b2bcb
tkhstty <tkhstty@gmail.com>
2023-02-04 11:54:21 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'cards/new'
+  devise_for :users
+  root 'items#index'
+  resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
+  resources :items, only: :order do
+    post 'order', on: :member
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
